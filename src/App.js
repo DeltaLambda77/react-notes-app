022a1ac8805780b56ff6fb0c87c7723529723d15
danlozano77 <danlozano77.gmail.com>
2022-05-25 16:41:51 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Editor from "./components/Editor.js"
+import Sidebar from "./components/Sidebar.js"
+import Data from "./data.js"
+import Split from "react-split"
+import {nanoid} from "nanoid"
+
+export default function App() {
+    return (
+        <main>
+            
+        </main>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,15 @@
+export const data = [
+    {
+        id: 1, body: `###This is the title
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet lacus sit amet erat faucibus consectetur. 
+        Cras in vulputate nunc, nec mollis mi. Integer sodales ante a bibendum auctor. Aenean euismod felis mauris, 
+        at porta elit lobortis in. Aliquam eget nulla purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+         per inceptos himenaeos. Donec vestibulum sem in erat tempus viverra. Nulla in erat sed sem varius consequat.
+        `,
+    },
+    { id: 2, body: `a`,},
+    { id: 3, body: `b`,},
+    { id: 4, body: `c`,},
+    { id: 5, body: `d`,},
+    { id: 6, body: `e`,},
+]
